c9c66dc7b1b054429eae0583633044f958637bc1
Daniel Gozalo <dgozalo@cloudbees.com>
2020-03-16 12:46:18 +0100

    feat: adding aws-iam-authenticator installation

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -15,6 +15,17 @@ function cleanup()
 
 trap cleanup EXIT
 
+echo "Installing aws-iam-authenticator"
+# Install aws-iam-authenticator to be able to connect to the cluster
+curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+
+# Checking installation
+aws-iam-authenticator help
+
+
 echo "Initializing modules..."
 terraform init
 
