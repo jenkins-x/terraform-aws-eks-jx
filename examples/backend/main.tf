afc8cba1c13c4109466866398966f17a4e0f1647
ankitm123 <ankitmohapatra123@gmail.com>
2020-07-21 16:16:16 -0400

    fix: add recursive flag to terraform fmt
    Signed-off-by: ankitm123 <ankitmohapatra123@gmail.com>

--- examples/backend/main.tf
+++ examples/backend/main.tf
@@ -1,14 +1,14 @@
- terraform {
+terraform {
   # https://www.terraform.io/docs/backends/types/s3.html
   backend "s3" {
-      region         = "<region>"
-      bucket         = "<s3-bucket-name>"
-      key            = "terraform.tfstate"
-      dynamodb_table = "terraform-state-lock"
-      encrypt        = true
-    }
- }
+    region         = "<region>"
+    bucket         = "<s3-bucket-name>"
+    key            = "terraform.tfstate"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
 
 module "eks-jx" {
-  source  = "jenkins-x/eks-jx/aws"
+  source = "jenkins-x/eks-jx/aws"
 }
