4b27a0719c87ff0f64703375767e55f50bd7c4b9
Alexandre Pauwels <8890201+ajpauwels@users.noreply.github.com>
2020-10-02 20:04:05 +0100

    fix: complete jx3 iam role name conversion

--- modules/cluster/local.tf
+++ modules/cluster/local.tf
@@ -10,5 +10,7 @@ locals {
   generated_seed         = random_string.suffix.result
   oidc_provider_url      = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
   jenkins-x-namespace    = "jx"
+  cluster_trunc          = substr("${var.cluster_name}", 0, 20)
+  jx_namespace_trunc     = substr("${local.jenkins-x-namespace}", 0, 20)
   cert-manager-namespace = "cert-manager"
 }
