5d80d3c5c60e07cac11262e5213fe24ceed6a0c3
ankitm123 <ankitmohapatra123@gmail.com>
2020-11-10 08:59:16 -0500

    feat: install kuberhealthy components
    Signed-off-by: ankitm123 <ankitmohapatra123@gmail.com>

--- variables.tf
+++ variables.tf
@@ -424,3 +424,9 @@ variable "use_asm" {
   type        = bool
   default     = false
 }
+
+variable "install_kuberhealthy" {
+  description = "Flag to specify if kuberhealthy operator should be installed"
+  type        = bool
+  default     = true
+}
