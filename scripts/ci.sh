dd6da67d9c24566e0178c73f306aff75ad90bc27
James Strachan <james.strachan@gmail.com>
2021-02-08 14:50:46 +0000

    chore: use new image

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,12 +9,12 @@ apk add curl
 # Checking AWS Installation
 aws --version
 
-echo "Installing aws-iam-authenticator"
+#echo "Installing aws-iam-authenticator"
 # Install aws-iam-authenticator to be able to connect to the cluster
-curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
-chmod +x ./aws-iam-authenticator
-mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+#curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
+#chmod +x ./aws-iam-authenticator
+#mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+#echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 
 # Checking installation
 aws-iam-authenticator help
