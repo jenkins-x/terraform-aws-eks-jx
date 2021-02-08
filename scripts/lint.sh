061fa244b515637808bced325c49085634d4d7f1
James Strachan <james.strachan@gmail.com>
2021-02-08 12:13:39 +0000

    fix: use upstream terraform image

---
+++ scripts/lint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -u
+
+echo "linting terraform"
+
+terraform init
+terraform version
+terraform validate
+
---
+++ scripts/security.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -u
+
+echo "terraform security check"
+
+export TFSEC="tfsec"
+
+curl -L "https://github.com/tfsec/tfsec/releases/download/v0.37.3/tfsec-linux-amd64" > $TFSEC
+chmod +x $TFSEC
+
+$TFSEC --version
+
+$TFSEC -e AWS002,AWS017
