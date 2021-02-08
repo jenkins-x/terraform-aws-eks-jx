ed29f7dd42eab48ed8365b4145ec5692c48e508f
James Strachan <james.strachan@gmail.com>
2021-02-08 09:24:45 +0000

    WIP: try upgrade aws provider

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.17, < 0.15"
 
   required_providers {
-    aws        = ">= 2.53.0, < 4.0"
+    aws        = "~> 3.0, < 4.0"
     kubernetes = "1.13.3"
     local      = "~> 1.2"
     null       = "~> 2.1"
