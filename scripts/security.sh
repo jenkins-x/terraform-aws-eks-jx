cc5efc0c482b22dbd938142a4b08063fa39d4d35
James Strachan <james.strachan@gmail.com>
2021-02-09 07:31:50 +0000

    chore: fix for security check

--- scripts/security.sh
+++ scripts/security.sh
@@ -5,11 +5,10 @@ set -u
 
 echo "terraform security check"
 
-export TFSEC="tfsec"
+#export TFSEC="tfsec"
+#curl -L "https://github.com/tfsec/tfsec/releases/download/v0.37.3/tfsec-linux-amd64" > $TFSEC
+#chmod +x $TFSEC
 
-curl -L "https://github.com/tfsec/tfsec/releases/download/v0.37.3/tfsec-linux-amd64" > $TFSEC
-chmod +x $TFSEC
+tfsec --version
 
-$TFSEC --version
-
-$TFSEC -e AWS002,AWS017
+tfsec -e AWS002,AWS017
