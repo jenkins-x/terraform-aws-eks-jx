a03506bdbb58d878ea32efc7d2e7f46b440e9b9d
ankitm123 <ankitmohapatra123@gmail.com>
2021-05-26 07:33:08 -0400

    feat: create nginx resources in terraform
    Signed-off-by: ankitm123 <ankitmohapatra123@gmail.com>

--- examples/jx3/main.tf
+++ examples/jx3/main.tf
@@ -1,6 +1,15 @@
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+}
+
+
 module "eks-jx" {
   source               = "../../"
   vault_user           = var.vault_user
   is_jx2               = false
   install_kuberhealthy = true
+  create_nginx         = true
+  cluster_version      = "1.20"
+  nginx_chart_version  = "3.12.0"
 }
