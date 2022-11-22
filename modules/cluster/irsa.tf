1a6fb0573d2377686ea18009c8cb539a794ea4e6
Mårten Svantesson <marten.svantesson@ticket.se>
2022-11-22 14:43:26 +0100

    fix: add permissions needed for cluster autoscaler 1.24 (#357)

--- modules/cluster/irsa.tf
+++ modules/cluster/irsa.tf
@@ -286,7 +286,11 @@ data "aws_iam_policy_document" "cluster_autoscaler" {
       "autoscaling:DescribeAutoScalingInstances",
       "autoscaling:DescribeLaunchConfigurations",
       "autoscaling:DescribeTags",
+      "ec2:DescribeInstanceTypes",
       "ec2:DescribeLaunchTemplateVersions",
+      "ec2:DescribeImages",
+      "ec2:GetInstanceTypesFromInstanceRequirements",
+      "eks:DescribeNodegroup"
     ]
 
     resources = ["*"]
