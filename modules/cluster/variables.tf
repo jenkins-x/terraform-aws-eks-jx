73ba7818a920769fad8a81d9d8de4bb85d303d2f
Mårten Svantesson <Marten.Svantesson@ticket.se>
2024-07-17 15:09:50 +0200

    chore!: remove creation of cluster from module
    BREAKING CHANGE: vpc and cluster now need to be created separately and be supplied to this module

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -8,122 +8,10 @@ variable "cluster_name" {
   type = string
 }
 
-variable "cluster_version" {
-  description = "Kubernetes version to use for the EKS cluster."
-  type        = string
-  default     = "1.26.6"
-}
-
-variable "eks_cluster_tags" {
-  description = "Add tags for the EKS Cluster"
-  type        = map
-  default     = {}
-}
-
 variable "profile" {
   description = "The AWS Profile used to provision the EKS Cluster"
   type        = string
   default     = null
-}
-
-# Worker Nodes
-variable "desired_node_count" {
-  type    = number
-  default = 3
-}
-
-variable "min_node_count" {
-  type    = number
-  default = 3
-}
-
-variable "max_node_count" {
-  type    = number
-  default = 5
-}
-
-variable "node_machine_type" {
-  type    = string
-  default = "m5.large"
-}
-
-# VPC
-variable "vpc_name" {
-  type    = string
-  default = "tf-vpc-eks"
-}
-
-variable "public_subnets" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "private_subnets" {
-  type    = list(string)
-  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-}
-
-variable "vpc_cidr_block" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "enable_nat_gateway" {
-  type    = bool
-  default = false
-}
-
-variable "single_nat_gateway" {
-  type    = bool
-  default = false
-}
-
-variable "spot_price" {
-  description = "The spot price ceiling for spot instances"
-  type        = string
-  default     = "0.1"
-}
-
-variable "node_groups" {
-  description = "List of node groups to be created"
-  type        = any
-  default     = {}
-}
-
-variable "node_group_ami" {
-  description = "ami type for the node group worker intances"
-  type        = string
-  default     = "AL2_x86_64"
-}
-
-variable "node_group_disk_size" {
-  description = "node group worker disk size"
-  type        = string
-  default     = "50"
-}
-
-variable "key_name" {
-  description = "The ssh key pair name to use"
-  type        = string
-  default     = ""
-}
-
-variable "volume_type" {
-  description = "The volume type to use. Can be standard, gp2 or io1"
-  type        = string
-  default     = "gp2"
-}
-
-variable "volume_size" {
-  description = "The volume size in GB"
-  type        = number
-  default     = 50
-}
-
-variable "iops" {
-  description = "The IOPS value"
-  type        = number
-  default     = 0
 }
 // ----------------------------------------------------------------------------
 // Flag Variables
@@ -155,18 +43,6 @@ variable "force_destroy" {
   default     = false
 }
 
-variable "enable_spot_instances" {
-  description = "Flag to enable spot instances"
-  type        = bool
-  default     = false
-}
-
-variable "cluster_in_private_subnet" {
-  description = "Flag to enable installation of cluster on private subnets"
-  type        = bool
-  default     = false
-}
-
 variable "use_kms_s3" {
   description = "Flag to determine whether kms should be used for encrypting s3 buckets"
   type        = bool
@@ -185,39 +61,6 @@ variable "s3_extra_tags" {
   default     = {}
 }
 
-
-variable "map_accounts" {
-  description = "Additional AWS account numbers to add to the aws-auth configmap."
-  type        = list(string)
-  default     = []
-}
-
-variable "map_roles" {
-  description = "Additional IAM roles to add to the aws-auth configmap."
-  type = list(object({
-    rolearn  = string
-    username = string
-    groups   = list(string)
-  }))
-  default = []
-}
-
-variable "map_users" {
-  description = "Additional IAM users to add to the aws-auth configmap."
-  type = list(object({
-    userarn  = string
-    username = string
-    groups   = list(string)
-  }))
-  default = []
-}
-
-variable "enable_key_name" {
-  description = "Flag to enable ssh key pair name"
-  type        = bool
-  default     = false
-}
-
 variable "s3_kms_arn" {
   description = "ARN of the kms key used for encrypting s3 buckets"
   type        = string
@@ -228,60 +71,6 @@ variable "content" {
   description = "Interpolated jx-requirements.yml"
   type        = string
   default     = ""
-}
-
-variable "cluster_endpoint_private_access" {
-  description = "Indicates whether or not the Amazon EKS private API server endpoint is enabled."
-  type        = bool
-  default     = false
-}
-
-variable "cluster_endpoint_private_access_cidrs" {
-  description = "List of CIDR blocks which can access the Amazon EKS private API server endpoint, when public access is disabled."
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
-}
-
-variable "cluster_endpoint_public_access" {
-  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled."
-  type        = bool
-  default     = true
-}
-
-variable "cluster_endpoint_public_access_cidrs" {
-  description = "List of CIDR blocks which can access the Amazon EKS public API server endpoint."
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
-}
-
-variable "allowed_spot_instance_types" {
-  description = "Allowed machine types for spot instances (must be same size)"
-  type        = any
-  default     = []
-}
-
-variable "enable_worker_groups_launch_template" {
-  description = "Flag to enable Worker Group Launch Templates"
-  type        = bool
-  default     = false
-}
-
-variable "lt_desired_nodes_per_subnet" {
-  description = "The number of worker nodes in each Subnet (AZ) if using Launch Templates"
-  type        = number
-  default     = 1
-}
-
-variable "lt_min_nodes_per_subnet" {
-  description = "The minimum number of worker nodes in each Subnet (AZ) if using Launch Templates"
-  type        = number
-  default     = 1
-}
-
-variable "lt_max_nodes_per_subnet" {
-  description = "The maximum number of worker nodes in each Subnet (AZ) if using Launch Templates"
-  type        = number
-  default     = 2
 }
 
 variable "jx_git_operator_values" {
@@ -308,18 +97,6 @@ variable "jx_bot_token" {
   default     = ""
 }
 
-variable "create_eks" {
-  description = "Controls if EKS cluster and associated resources should be created or not. If you have an existing eks cluster, set it to false"
-  type        = bool
-  default     = true
-}
-
-variable "create_vpc" {
-  description = "Controls if VPC and related resources should be created"
-  type        = bool
-  default     = true
-}
-
 variable "vpc_id" {
   description = "The VPC to create EKS cluster in if create_vpc is false"
   type        = string
@@ -330,21 +107,6 @@ variable "subnets" {
   description = "The subnet ids to create EKS cluster in if create_vpc is false"
   type        = list(string)
   default     = []
-}
-
-variable "encrypt_volume_self" {
-  description = "Encrypt the ebs and root volume for the self managed worker nodes. This is only valid for the worker group launch template"
-  type        = bool
-  default     = false
-}
-
-variable "cluster_encryption_config" {
-  description = "Configuration block with encryption configuration for the cluster."
-  type = list(object({
-    provider_key_arn = string
-    resources        = list(string)
-  }))
-  default = []
 }
 
 variable "create_tekton_role" {
@@ -460,22 +222,4 @@ variable "boot_iam_role" {
 variable "enable_acl" {
   description = "Flag to enable ACL instead of bucket ownership for S3 storage"
   type        = bool
-}
-
-variable "create_addon_role" {
-  description = "Flag to control addon iam roles creation"
-  type        = bool
-  default     = false
-} 
-
-variable "enable_ebs_addon" {
-  description = "Flag to enable or disable EBS CSI driver addon"
-  type        = bool
-  default     = false
-}
-
-variable "ebs_addon_version" {
-  description = "EBS CSI driver addon (aws-ebs-csi-driver) version"
-  type = string
-  default = "v1.21.0-eksbuild.1"
 }
--- variables.tf
+++ variables.tf
@@ -19,11 +19,6 @@ variable "profile" {
   default     = null
 }
 
-variable "cluster_version" {
-  description = "Kubernetes version to use for the EKS cluster."
-  type        = string
-}
-
 // ----------------------------------------------------------------------------
 // Vault
 // ----------------------------------------------------------------------------
@@ -71,119 +66,6 @@ variable "velero_username" {
   description = "The username to be assigned to the Velero IAM user"
   type        = string
   default     = "velero"
-}
-
-// ----------------------------------------------------------------------------
-// Worker Nodes Variables
-// ----------------------------------------------------------------------------
-variable "desired_node_count" {
-  description = "The number of worker nodes to use for the cluster"
-  type        = number
-  default     = 3
-}
-
-variable "min_node_count" {
-  description = "The minimum number of worker nodes to use for the cluster"
-  type        = number
-  default     = 3
-}
-
-variable "max_node_count" {
-  description = "The maximum number of worker nodes to use for the cluster"
-  type        = number
-  default     = 5
-}
-
-variable "node_machine_type" {
-  description = "The instance type to use for the cluster's worker nodes"
-  type        = string
-  default     = "m5.large"
-}
-
-variable "spot_price" {
-  description = "The spot price ceiling for spot instances"
-  type        = string
-  default     = "0.1"
-}
-
-variable "node_group_ami" {
-  description = "ami type for the node group worker intances"
-  type        = string
-  default     = "AL2_x86_64"
-}
-
-variable "node_group_disk_size" {
-  description = "node group worker disk size"
-  type        = string
-  default     = "50"
-}
-
-variable "node_groups_managed" {
-  description = "List of managed node groups to be created and their respective settings"
-  type        = any
-  default     = { eks-jx-node-group = {} }
-}
-
-variable "key_name" {
-  description = "The ssh key pair name"
-  type        = string
-  default     = ""
-}
-
-variable "volume_type" {
-  description = "The volume type to use. Can be standard, gp2 or io1"
-  type        = string
-  default     = "gp2"
-}
-
-variable "volume_size" {
-  description = "The volume size in GB"
-  type        = number
-  default     = 50
-}
-
-variable "iops" {
-  description = "The IOPS value"
-  type        = number
-  default     = 0
-}
-// ----------------------------------------------------------------------------
-// VPC Variables
-// ----------------------------------------------------------------------------
-variable "vpc_name" {
-  description = "The name of the VPC to be created for the cluster"
-  type        = string
-  default     = "tf-vpc-eks"
-}
-
-variable "public_subnets" {
-  description = "The public subnet CIDR block to use in the created VPC"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "private_subnets" {
-  description = "The private subnet CIDR block to use in the created VPC"
-  type        = list(string)
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-}
-
-variable "vpc_cidr_block" {
-  description = "The vpc CIDR block"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "enable_nat_gateway" {
-  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
-  type        = bool
-  default     = false
-}
-
-variable "single_nat_gateway" {
-  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
-  type        = bool
-  default     = false
 }
 
 // ----------------------------------------------------------------------------
@@ -264,28 +146,10 @@ variable "force_destroy" {
   default     = false
 }
 
-variable "enable_spot_instances" {
-  description = "Flag to enable spot instances"
-  type        = bool
-  default     = false
-}
-
-variable "enable_worker_group" {
-  description = "Flag to enable worker group. Setting this to false will provision a node group instead"
-  type        = bool
-  default     = true
-}
-
 variable "enable_key_rotation" {
   description = "Flag to enable kms key rotation"
   type        = bool
   default     = true
-}
-
-variable "cluster_in_private_subnet" {
-  description = "Flag to enable installation of cluster on private subnets"
-  type        = bool
-  default     = false
 }
 
 variable "use_kms_s3" {
@@ -297,37 +161,6 @@ variable "use_kms_s3" {
 // ----------------------------------------------------------------------------
 // Cluster AWS Auth Variables
 // ----------------------------------------------------------------------------
-variable "map_accounts" {
-  description = "Additional AWS account numbers to add to the aws-auth configmap."
-  type        = list(string)
-  default     = []
-}
-
-variable "map_roles" {
-  description = "Additional IAM roles to add to the aws-auth configmap."
-  type = list(object({
-    rolearn  = string
-    username = string
-    groups   = list(string)
-  }))
-  default = []
-}
-
-variable "map_users" {
-  description = "Additional IAM users to add to the aws-auth configmap."
-  type = list(object({
-    userarn  = string
-    username = string
-    groups   = list(string)
-  }))
-  default = []
-}
-
-variable "enable_key_name" {
-  description = "Flag to enable ssh key pair name"
-  type        = bool
-  default     = false
-}
 
 variable "s3_kms_arn" {
   description = "ARN of the kms key used for encrypting s3 buckets"
@@ -351,60 +184,6 @@ variable "ignoreLoadBalancer" {
   default     = false
   type        = bool
   description = "Flag to specify if jx boot will ignore loadbalancer DNS to resolve to an IP"
-}
-
-variable "cluster_endpoint_private_access" {
-  description = "Indicates whether or not the Amazon EKS private API server endpoint is enabled."
-  type        = bool
-  default     = false
-}
-
-variable "cluster_endpoint_private_access_cidrs" {
-  description = "List of CIDR blocks which can access the Amazon EKS private API server endpoint, when public access is disabled."
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
-}
-
-variable "cluster_endpoint_public_access" {
-  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled."
-  type        = bool
-  default     = true
-}
-
-variable "cluster_endpoint_public_access_cidrs" {
-  description = "List of CIDR blocks which can access the Amazon EKS public API server endpoint."
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
-}
-
-variable "allowed_spot_instance_types" {
-  description = "Allowed machine types for spot instances (must be same size)"
-  type        = any
-  default     = []
-}
-
-variable "enable_worker_groups_launch_template" {
-  description = "Flag to enable Worker Group Launch Templates"
-  type        = bool
-  default     = false
-}
-
-variable "lt_desired_nodes_per_subnet" {
-  description = "The number of worker nodes in each Subnet (AZ) if using Launch Templates"
-  type        = number
-  default     = 1
-}
-
-variable "lt_min_nodes_per_subnet" {
-  description = "The minimum number of worker nodes in each Subnet (AZ) if using Launch Templates"
-  type        = number
-  default     = 1
-}
-
-variable "lt_max_nodes_per_subnet" {
-  description = "The maximum number of worker nodes in each Subnet (AZ) if using Launch Templates"
-  type        = number
-  default     = 2
 }
 
 variable "registry" {
@@ -435,18 +214,6 @@ variable "jx_bot_token" {
   description = "Bot token used to interact with the Jenkins X cluster git repository"
   type        = string
   default     = ""
-}
-
-variable "create_eks" {
-  description = "Controls if EKS cluster and associated resources should be created or not. If you have an existing eks cluster for jx, set it to false"
-  type        = bool
-  default     = true
-}
-
-variable "create_vpc" {
-  description = "Controls if VPC and related resources should be created. If you have an existing vpc for jx, set it to false"
-  type        = bool
-  default     = true
 }
 
 variable "vpc_id" {
@@ -489,21 +256,6 @@ variable "install_kuberhealthy" {
   description = "Flag to specify if kuberhealthy operator should be installed"
   type        = bool
   default     = false
-}
-
-variable "encrypt_volume_self" {
-  description = "Encrypt the ebs and root volume for the self managed worker nodes. This is only valid for the worker group launch template"
-  type        = bool
-  default     = false
-}
-
-variable "cluster_encryption_config" {
-  description = "Configuration block with encryption configuration for the cluster."
-  type = list(object({
-    provider_key_arn = string
-    resources        = list(string)
-  }))
-  default = []
 }
 
 variable "create_tekton_role" {
@@ -661,20 +413,4 @@ variable "enable_acl" {
   default     = false
 }
 
-variable "create_addon_role" {
-  description = "Flag to control addon iam roles creation"
-  type        = bool
-  default     = false
-}
 
-variable "enable_ebs_addon" {
-  description = "Flag to enable or disable EBS CSI driver addon"
-  type        = bool
-  default     = false
-}
-
-variable "ebs_addon_version" {
-  description = "EBS CSI driver addon (aws-ebs-csi-driver) version"
-  type = string
-  default = "v1.21.0-eksbuild.1"
-}
