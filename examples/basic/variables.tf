f449c144a6a761ef432be642d423f5b3004575e8
Mårten Svantesson <Marten.Svantesson@ticket.se>
2024-07-31 16:47:13 +0200

    docs: fix examples
    remove deprecated examples and polish up and rename the existing-clsuter example

--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -1,9 +1,171 @@
-variable "region" {
+variable "cluster_name" {
   type    = string
-  default = "us-east-1"
+  default = "existing-cluster"
 }
 
-variable "profile" {
-  type    = string
-  default = "default"
+variable "cluster_version" {
+  description = "Kubernetes version to use for the EKS cluster."
+  type        = string
+  default     = "1.30"
 }
+
+# Worker Nodes
+variable "desired_node_count" {
+  type    = number
+  default = 3
+}
+
+variable "min_node_count" {
+  type    = number
+  default = 3
+}
+
+variable "max_node_count" {
+  type    = number
+  default = 5
+}
+
+variable "node_machine_type" {
+  type    = string
+  default = "m5.large"
+}
+
+# VPC
+variable "vpc_name" {
+  type    = string
+  default = "tf-vpc-eks"
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "vpc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "enable_nat_gateway" {
+  type    = bool
+  default = false
+}
+
+variable "single_nat_gateway" {
+  type    = bool
+  default = false
+}
+
+variable "node_group_ami" {
+  description = "ami type for the node group worker intances"
+  type        = string
+  default     = "AL2023_x86_64_STANDARD"
+}
+
+// ----------------------------------------------------------------------------
+// Flag Variables
+// ----------------------------------------------------------------------------
+variable "enable_logs_storage" {
+  type    = bool
+  default = true
+}
+
+variable "enable_reports_storage" {
+  type    = bool
+  default = true
+}
+
+variable "enable_repository_storage" {
+  type    = bool
+  default = true
+}
+
+variable "force_destroy" {
+  description = "Flag to determine whether storage buckets get forcefully destroyed. If set to false, empty the bucket first in the aws s3 console, else terraform destroy will fail with BucketNotEmpty error"
+  type        = bool
+  default     = false
+}
+
+
+variable "cluster_in_private_subnet" {
+  description = "Flag to enable installation of cluster on private subnets"
+  type        = bool
+  default     = false
+}
+
+variable "map_accounts" {
+  description = "Additional AWS account numbers to add to the aws-auth configmap."
+  type        = list(string)
+  default     = []
+}
+
+variable "map_roles" {
+  description = "Additional IAM roles to add to the aws-auth configmap."
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+  default = []
+}
+
+variable "map_users" {
+  description = "Additional IAM users to add to the aws-auth configmap."
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+  default = []
+}
+
+variable "cluster_endpoint_private_access" {
+  description = "Indicates whether or not the Amazon EKS private API server endpoint is enabled."
+  type        = bool
+  default     = false
+}
+
+variable "cluster_endpoint_public_access" {
+  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled."
+  type        = bool
+  default     = true
+}
+
+variable "jx_git_url" {
+  description = "URL for the Jenins X cluster git repository"
+  type        = string
+  default     = ""
+}
+
+variable "jx_bot_username" {
+  description = "Bot username used to interact with the Jenkins X cluster git repository"
+  type        = string
+  default     = ""
+}
+
+variable "jx_bot_token" {
+  description = "Bot token used to interact with the Jenkins X cluster git repository"
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "use_vault" {
+  type    = bool
+  default = false
+}
+
+variable "use_asm" {
+  type    = bool
+  default = true
+}
+
