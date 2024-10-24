723298e82cefaa8aa2797bfac1de262260d0ea94
Mårten Svantesson <Marten.Svantesson@ticket.se>
2024-10-24 17:39:55 +0200

    chore: upgrade jx-git-operator

--- modules/cluster/charts.tf
+++ modules/cluster/charts.tf
@@ -3,7 +3,7 @@ resource "helm_release" "jx-git-operator" {
   chart            = "jx-git-operator"
   namespace        = "jx-git-operator"
   repository       = "https://jenkins-x-charts.github.io/repo"
-  version          = "0.1.7"
+  version          = "0.2.1"
   create_namespace = true
 
   values = var.jx_git_operator_values
